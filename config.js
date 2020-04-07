fec4c1a5c56f55ec622adabd36aca0506d849fa7
Eliott-2098 <62996087+Eliott-2098@users.noreply.github.com>
2020-04-07 11:34:00 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,2 @@
+exports.TOKEN = "Njk0NTY5NTM1OTg2MjA0Njcy.XoxCeA.pzUCF9vKOpqt4g0CZ2ymDg_U3uw";
+exports.PREFIX = "!";
---
+++ main.js
@@ -0,0 +1,14 @@
+const Discord = require("discord.js");
+const { TOKEN , PREFIX } = require("./config");
+const client = new Discord.Client({disableMentions: "everyone"});
+
+client.on("ready", () => {
+  console.log("Je suis prêt !");
+});
+
+client.on("message", msg => {
+  if (msg.content.startsWith(`${PREFIX}ping`)) msg.channel.send("Pong!");
+  if (msg.content.startsWith(`${PREFIX}pong`)) msg.channel.send("Ping!");
+});
+
+client.login(TOKEN);
